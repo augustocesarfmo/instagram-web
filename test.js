446223724f74dce836ecfc5435c56b624a915875
Augusto Oliveira <augustocesarfmo@gmail.com>
2024-09-02 16:55:02 -0300

    feat: implementa um exemplo de like

--- test.js
+++ test.js
@@ -1,3 +1,20 @@
-var p = document.createElement("p");
-p.textContent = "Meu texto";
-document.body.appendChild(p);
+var isLiked = false;
+function like() {
+    var button = document.getElementById("btn-like");
+    var icon = button === null || button === void 0 ? void 0 : button.children[0];
+    if (!icon)
+        return;
+    // Remove o coração preenchido e adiciona o coração vazio
+    if (isLiked) {
+        icon.classList.remove("fa-heart");
+        icon.classList.remove("liked");
+        icon.classList.add("fa-heart-o");
+    }
+    else {
+        // Remove o coração vazio e adiciona o coração preenchido
+        icon.classList.remove("fa-heart-o");
+        icon.classList.add("fa-heart");
+        icon.classList.add("liked");
+    }
+    isLiked = !isLiked; // Inverte o valor de `isLiked`
+}
