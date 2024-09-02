62ff0a8e47da2db966387cd259420bd3c8cb5497
Augusto Oliveira <augustocesarfmo@gmail.com>
2024-09-02 11:58:56 -0300

    feat: implementa o botão de like

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
-console.log("funcionou");
-
-// Cria um novo elemento <p>
-const novoParagrafo = document.createElement("p");
-
-// Adiciona algum texto ao parágrafo
-novoParagrafo.textContent = "Este é um parágrafo criado com JavaScript.";
-
-// Opcional: Adiciona uma classe ao parágrafo
-novoParagrafo.className = "meu-paragrafo";
-
-// Adiciona o parágrafo ao corpo do documento
-document.body.appendChild(novoParagrafo);
-
-document.getElementById("demo").innerHTML = "Hello World!";
+var isLiked = false;
+function like() {
+    isLiked = !isLiked;
+    var btnLike = document.getElementsByClassName("btn-like")[0];
+    var icon = btnLike.children[0];
+    if (isLiked) {
+        icon.classList.remove("fa-heart-o");
+        icon.classList.add("liked");
+        icon.classList.add("fa-heart");
+        return;
+    }
+    icon.classList.remove("fa-heart");
+    icon.classList.remove("liked");
+    icon.classList.add("fa-heart-o");
+}
--- test.ts
+++ test.ts
@@ -1,2 +1,20 @@
-console.log("funcionou");
-document.getElementById("demo").innerHTML = "Hello World!";
+let isLiked = false;
+
+function like() {
+  isLiked = !isLiked;
+
+  const btnLike = document.getElementsByClassName("btn-like")[0];
+  const icon = btnLike.children[0];
+
+  if (isLiked) {
+    icon.classList.remove("fa-heart-o");
+    icon.classList.add("liked");
+    icon.classList.add("fa-heart");
+
+    return;
+  }
+
+  icon.classList.remove("fa-heart");
+  icon.classList.remove("liked");
+  icon.classList.add("fa-heart-o");
+}
