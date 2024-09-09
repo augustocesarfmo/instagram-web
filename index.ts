e4b357dd7211529989a711b14d63647fe645cb81
Augusto Oliveira <augustocesarfmo@gmail.com>
2024-09-09 11:48:19 -0300

    feat: torna o botão de like dinâmico

--- index.ts
+++ index.ts
@@ -24,13 +24,72 @@ class Post {
   }
 
   like() {
+    const post = document.getElementById(this._id);
+    const btnLike = post?.querySelector(".btn-like");
+
+    if (!btnLike) return;
+
     this._isLiked = !this._isLiked;
 
     if (this._isLiked === true) {
       this._numberOfLikes += 1;
+      // Renderizar o ícone de coração preenchido
     } else {
       this._numberOfLikes -= 1;
+      // Renderizar o ícone de coração não preenchido
     }
+  }
+
+  toHTML() {
+    const postContainer = document.createElement("div");
+    postContainer.className = "post-container";
+    postContainer.id = this._id;
+
+    const postHeader = `
+      <div class="post-header">
+        <div class="left">
+          <div>
+            <img title="Avatar image" src=${this._avatarUrl}>
+          </div>
+
+          <span>${this._userName}</span>
+        </div>
+
+        <div class="right">
+          follow ...
+        </div>
+      </div>`;
+
+    const postImage = `
+      <div class="post-image">
+        <img title="Post image" src=${this._imageUrl}>
+      </div>
+    `;
+
+    const postIcons = `
+      <div class="post-icons">
+        <div class="btn btn-like">
+          <i class="fa fa-heart-o"></i>
+          <!-- <i class="fa fa-heart liked"></i> -->
+        </div>
+        <div class="btn">
+          <i class="fa fa-comment-o"></i>
+        </div>
+        <div class="btn">
+          <i class="fa fa-paper-plane-o"></i>
+        </div>
+      </div>
+    `;
+
+    postContainer.innerHTML = postHeader;
+    postContainer.innerHTML += postImage;
+    postContainer.innerHTML += postIcons;
+
+    postContainer
+      .querySelector(".btn-like")
+      ?.addEventListener("click", () => this.like());
+
+    document.body.appendChild(postContainer);
   }
 }
 
@@ -44,8 +103,6 @@ for (let index = 0; index < 15; index++) {
     faker.lorem.paragraph()
   );
 
-  posts.push(post);
+  post.toHTML();
+  // posts.push(post);
 }
-
-console.log(posts);
-console.log("testando");
